fb1e63883edaf9bfca1f2e6a9b4c9dff891a525f
Dafantasticfish <41270468+Dafantasticfish@users.noreply.github.com>
2018-07-26 23:18:15 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -133,13 +133,13 @@ async def er(ctx):
 @client.event
 async def on_reaction_add(reaction,user):
 	if reaction.message.id in active_messages and reaction.emoji in reaction_roles and user != client.user:
-	role=discord.utils.get(reaction.message.server.roles,id=reaction_roles[reaction.emoji])
-	await client.remove_reaction(reaction.message,reaction.emoji,user)
+		role=discord.utils.get(reaction.message.server.roles,id=reaction_roles[reaction.emoji])
                 for r_id in reaction_roles.values():
                     e_role=discord.utils.get(reaction.message.server.roles,id=r_id)
                     if e_role in user.roles:
                         await client.remove_roles(user,e_role)
-                        await client.add_roles(user,role)
+                await client.remove_reaction(reaction.message,reaction.emoji,user)
+                await client.add_roles(user,role)
 
 #------------------------------------Token-----------------------------------
 
