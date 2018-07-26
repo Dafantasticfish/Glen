2740c30363f30f343931dd843a9b764f70e0348d
Dafantasticfish <41270468+Dafantasticfish@users.noreply.github.com>
2018-07-26 18:52:35 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -7,10 +7,34 @@ import os
 import os,json
 import edit_json,read_json
 
+def read_json(file_name):
+	if file_name.endswith('.json')==False:
+		file_name=file_name+'.json'
+	if not os.path.isfile(file_name):
+		list_name=open(file_name,"w+")
+		list_name={}
+	else:
+		try:
+			with open(file_name) as f:
+				list_name = json.load(f)
+		except ValueError:
+			list_name={}
+	return list_name
 
+def edit_json(file_name,items):
+	if file_name.endswith('.json')==False:
+		file_name=file_name+'.json'
+	with open(file_name,"w") as f:
+		json.dump(items,f)
 
 Client = discord.Client()
 client = commands.Bot(command_prefix = "!")
+
+
+
+
+
+
 
 @client.event
 async def on_ready():
@@ -125,25 +149,7 @@ async def on_reaction_add(reaction,user):
 		await bot.add_roles(user,role)
 
 
-def read_json(file_name):
-	if file_name.endswith('.json')==False:
-		file_name=file_name+'.json'
-	if not os.path.isfile(file_name):
-		list_name=open(file_name,"w+")
-		list_name={}
-	else:
-		try:
-			with open(file_name) as f:
-				list_name = json.load(f)
-		except ValueError:
-			list_name={}
-	return list_name
 
-def edit_json(file_name,items):
-	if file_name.endswith('.json')==False:
-		file_name=file_name+'.json'
-	with open(file_name,"w") as f:
-		json.dump(items,f)
 
 
 
