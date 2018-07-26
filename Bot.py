a771d4ad8e481fcf8808ae795d4081d7a501d20a
Dafantasticfish <41270468+Dafantasticfish@users.noreply.github.com>
2018-07-26 23:26:28 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -55,8 +55,7 @@ async def on_member_remove(member):
     channel = discord.utils.get(member.server.channels, name = 'goodbye')
     emb = (discord.Embed(title = " Goodbye :frowning2:  ", color = 484848))
     emb.set_author(name = member.display_name+" Left", icon_url = member.avatar_url)
-    
-    await client.send_message(channel, embed=emb)    
+        await client.send_message(channel, embed=emb)    
 
 @client.event
 async def on_member_join(member):
@@ -132,14 +131,11 @@ async def er(ctx):
 
 @client.event
 async def on_reaction_add(reaction,user):
-	if reaction.message.id in active_messages and reaction.emoji in reaction_roles and user != client.user:
-		role=discord.utils.get(reaction.message.server.roles,id=reaction_roles[reaction.emoji])
-		for r_id in reaction_roles.values():
-			e_role=discord.utils.get(reaction.message.server.roles,id=r_id)
-			if e_role in user.roles:
-				await client.remove_roles(user,e_role)
-		await client.remove_reaction(reaction.message,reaction.emoji,user)
-		await client.add_roles(user,role)
+    if reaction.message.id in active_messages and reaction.emoji in reaction_roles and user != client.user:
+        role=discord.utils.get(reaction.message.server.roles,id=reaction_roles[reaction.emoji])
+        await client.remove_reaction(reaction.message,reaction.emoji,user)
+        await client.add_roles(user,role)
+
 		
 #------------------------------------Token-----------------------------------
 
