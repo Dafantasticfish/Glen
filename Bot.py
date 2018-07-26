9b36a141a508e8355dcfff406d8f7ae6ec72d65b
Dafantasticfish <41270468+Dafantasticfish@users.noreply.github.com>
2018-07-26 23:28:27 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -55,7 +55,7 @@ async def on_member_remove(member):
     channel = discord.utils.get(member.server.channels, name = 'goodbye')
     emb = (discord.Embed(title = " Goodbye :frowning2:  ", color = 484848))
     emb.set_author(name = member.display_name+" Left", icon_url = member.avatar_url)
-        await client.send_message(channel, embed=emb)    
+    await client.send_message(channel, embed=emb)    
 
 @client.event
 async def on_member_join(member):
