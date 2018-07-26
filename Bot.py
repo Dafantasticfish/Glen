7272dc9cadc176403ee5a7121798961fa8d54c9c
Dafantasticfish <41270468+Dafantasticfish@users.noreply.github.com>
2018-07-26 17:18:51 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -37,6 +37,14 @@ async def join(ctx):
     channel = ctx.message.author.voice.voice_channel
     await client.join_voice_channel(channel)
 
+@client.command(pass_context=True)
+async def leave(ctx):
+    server = ctx.message.server
+    voice_client = client.voice_client_in(server)
+    await voice_client.disconnect()
+
+
+
 
 
 
