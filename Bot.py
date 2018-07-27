013e2b669df6316d2854601fe1609103620abf7b
Dafantasticfish <41270468+Dafantasticfish@users.noreply.github.com>
2018-07-27 18:22:47 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -39,7 +39,24 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     for server in client.servers:
-        print(server.name)
+    print(server.name)
+
+#----------music bot---------------------------------------
+
+@client.command(pass_context=True)
+async def join(ctx):
+    channel = ctx.message.author.voice.voice_channel
+    await client.join_voice_channel(channel)
+
+@client.command(pass_context=True)
+async def leave(ctx):
+    server = ctx.message.server
+    voice_client = client.voice_client_in(server)
+    await voice_client.disconnect()
+
+
+
+#reaction role----------------------------------------------------
 
 @client.event
 async def on_member_join(member):
